142e49c3b39b1d3aed6b03c44093b139ef1d75ac
tss <tss@debian-tss24>
2016-05-19 19:24:27 +0200

    Prima revisione

---
+++ src/lezionegit/LezioneGIT.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lezionegit;
+
+/**
+ * Progetto di studio su controllo della versione con git
+ *
+ * @author Matteo Demarchi
+ */
+public class LezioneGIT {
+
+    /*
+    
+     - CLIENT - SERVER -
+     svn:
+     mercurial:
+    
+     - LOCALE (PeerToPeer) -
+     git:    salva tutto in locale, ma può anche essere messo su server
+     git offre un server gratuito (repository server di softwares opensource)
+     non si paga, ma il codice è pubblico! GitHub - https://github.com/
+    
+     MyAccount su GitHub:
+     JhonnyPala
+     leonidadegliagiadi@gmail.com
+     leonida400
+     */
+    /**
+     * Funzione main
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Il progetto è partito!");
+    }
+
+}
